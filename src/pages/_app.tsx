1ad21a380ca1ce018b0135f2c93746174f2b97ab
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-08-31 22:51:39 +0900

    [Update] `_app.tsx`에 헤더, 푸터 및 테마 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
+import { Footer } from "../components/Footer.tsx/Footer";
+import { Header } from "../components/header/Header";
+import { useThemeInLocalStorage } from "../hooks/useThemeInLocalStorage";
 import GlobalStyle from "../styles/GlobalStyle";
 import { darkTheme, lightTheme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { isDarkMode, toggleTheme } = useThemeInLocalStorage();
+
   return (
     <>
       <Head>
@@ -13,7 +18,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <GlobalStyle />
+        <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </>
   );
